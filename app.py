90eb6a1ae3f49c5f81333b4ebf7ef6ae2dce44d0
Anjali Gupta <63281911+guptanjali16@users.noreply.github.com>
2025-02-17 14:53:12 +0000

    Added Flask Project configs

--- app.py
+++ app.py
@@ -6,6 +6,8 @@ app = Flask(__name__)
 def hello_world():
     return 'Hello, World!'
 
-if __name__ == '__main__':  
-   app.run(debug=True)
+print(__name__)
 
+if __name__ == '__main__':
+   print('im inside main')
+   app.run(host='0.0.0.0', debug=True)
